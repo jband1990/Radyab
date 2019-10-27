70bf7d32f25b9f54aec513534d9c2590cf81557c
hadi <Hmcsf.co@yahoo.com>
2019-10-27 22:10:28 +0330

    final 10/27/2019

--- getData.php
+++ getData.php
@@ -2,6 +2,11 @@
 //connect to the database
 $connection= mysqli_connect('localhost','root','');
 mysqli_select_db($connection,'radyab');
+// Check connection
+if (mysqli_connect_errno())
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
 //file_get_contents('device/00001.txt');
 //file_get_contents('device/00001.txt'));
 $list= scandir('device');
@@ -17,7 +22,17 @@ foreach ($list as $file){
     //die();
     foreach ($listPosition as $index => $position){
         $listPosition [$index]= explode(',',$position);
+        //seprate date and time with preg_match
+       ;
+        preg_match('/([0-9]{4})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/', $listPosition [$index][4],$result);
+        $date=$result[1].'-'.$result[2].'-'.$result[3].' '.$result[4].':'.$result[5].':'.$result[6];
+        var_dump($date);
+        die();
     }
-    var_dump($listPosition);
-    die();
+   /* mysqli_query(INSERT INTO `position`
+    (`longitude`, `latitiude`, `speed`, `createDate`)
+    VALUES ('2', '3', '4', '2019-10-16 00:00:00');)
+   */
+    //var_dump($listPosition);
+    //die();
 }
