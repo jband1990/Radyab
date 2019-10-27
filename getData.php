3628c66666fc2f28cffaa51a8ae526e8b248e72e
hadi <Hmcsf.co@yahoo.com>
2019-10-27 13:51:55 +0330

    seperate recived location with ---

--- getData.php
+++ getData.php
@@ -1,13 +1,20 @@
 <?php
 //file_get_contents('device/00001.txt');
-var_dump(file_get_contents('device/00001.txt'));
+//file_get_contents('device/00001.txt'));
 $list= scandir('device');
 unset($list[0]);
 unset($list[1]);
-var_dump($list);
-/**
- * Created by PhpStorm.
- * User: msi
- * Date: 10/25/2019
- * Time: 2:48 PM
- */
+//var_dump($list);
+//seperate recivied locations
+foreach ($list as $file){
+    $content=file_get_contents('device/'.$file);
+        $listPosition=explode('---',$content);
+        //var_dump($content);
+    //var_dump($listPosition);
+    //die();
+    foreach ($listPosition as $index => $position){
+        $listPosition [$index]= explode(',',$position);
+    }
+    var_dump($listPosition);
+    die();
+}
